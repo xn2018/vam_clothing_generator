c1843aa7e204575ff3ea91562d80d5d7e676d7a8
xn2018 <lovie190@hotmail.com>
2026-08-13 00:51:47 +0800

    clothing normal fix

--- wrap/DeriveMeshes.py
+++ wrap/DeriveMeshes.py
@@ -69,107 +69,91 @@ def build_uv_mesh(
     mesh: bpy.types.Mesh,
     uv_layer: bpy.types.MeshUVLoopLayer,
 ):
-    #
-    # Base vertex count
-    #
     base_vertex_count = len(mesh.vertices)
-    ##########################################################
-    # UV Vertices
     #
-    # IMPORTANT:
-    # UV vertex index is independent from base vertex index
-    ##########################################################
-    uv_vertices = []
-    ##########################################################
-    # Original UV coordinates
-    ##########################################################
-    orig_uv = []
-    ##########################################################
-    # Mapping:
+    # UVVertices
     #
-    # UV vertex -> base vertex
+    uv_vertices = [
+        mesh.vertices[i].co.copy()
+        for i in range(base_vertex_count)
+    ]
     #
-    ##########################################################
+    # OrigUV
+    #
+    orig_uv = [
+        Vector((0.0, 0.0))
+        for _ in range(base_vertex_count)
+    ]
+    #
+    # VertexMap
+    #
     vertex_maps = []
-    ##########################################################
-    # Cache:
     #
-    # (base vertex, u, v)
-    #       |
-    #       v
-    # uv vertex index
+    # (baseIndex,u,v)->uvIndex
     #
-    ##########################################################
     uv_map = {}
-    ##########################################################
+    #
     # first uv vertex of base vertex
     #
-    # used by DAZ duplicate mapping
-    ##########################################################
     first_uv = {}
-    ##########################################################
-    # UV polygons
-    ##########################################################
+    #
+    # UV PolyList
+    #
     uv_poly_list = []
-    ##########################################################
-    # Build UV mesh
-    ##########################################################
+    #
+    # Build
+    #
     for poly in mesh.polygons:
         uv_indices = []
         for loop_index in poly.loop_indices:
             loop = mesh.loops[loop_index]
             base_index = loop.vertex_index
             uv = uv_layer.data[loop_index].uv
-            #
-            # UV vertex identity
-            #
             key = (
                 base_index,
-                round(uv.x,8),
-                round(uv.y,8),
+                round(uv.x, 8),
+                round(uv.y, 8),
             )
             #
-            # Existing UV vertex
+            # Existing
             #
             if key in uv_map:
                 uv_index = uv_map[key]
-            #
-            # New UV vertex
-            #
             else:
                 #
-                # ALWAYS create new UV vertex
-                #
-                uv_index = len(uv_vertices)
-                uv_map[key] = uv_index
-                #
-                # Position
-                #
-                uv_vertices.append(
-                    mesh.vertices[
-                        base_index
-                    ].co.copy()
-                )
-                #
-                # UV
-                #
-                orig_uv.append(
-                    uv.copy()
-                )
-                #
-                # First UV of this base vertex
+                # First UV vertex of this base vertex
                 #
                 if base_index not in first_uv:
+                    uv_index = base_index
                     first_uv[base_index] = uv_index
+                    uv_map[key] = uv_index
+                    orig_uv[uv_index] = uv.copy()
                 #
                 # Duplicate UV vertex
                 #
                 else:
+                    uv_index = len(
+                        uv_vertices
+                    )
+                    uv_map[key] = uv_index
+                    uv_vertices.append(
+                        mesh.vertices[
+                            base_index
+                        ].co.copy()
+                    )
+                    orig_uv.append(
+                        uv.copy()
+                    )
                     vertex_maps.append(
                         MeshtoUVMapping(
-                            fromvert=first_uv[base_index],
-                            tovert=uv_index,
-                            polyindex=poly.index,
+                            fromvert=
+                                first_uv[
+                                    base_index
+                                ],
+                            tovert=
+                                uv_index,
+                            polyindex=
+                                poly.index,
                         )
                     )
             uv_indices.append(
@@ -181,7 +165,18 @@ def build_uv_mesh(
                 vertices=uv_indices,
             )
         )
-
+    #
+    # Official:
+    # duplicate positions
+    #
+    for vm in vertex_maps:
+        uv_vertices[
+            vm.tovert
+        ] = (
+            uv_vertices[
+                vm.fromvert
+            ].copy()
+        )
     return (
         uv_vertices,
         orig_uv,
@@ -278,70 +273,56 @@ def calculate_vertex_normals(
     vertex_count: int,
     triangles: list[int],
     surface_normals: list[Vector],
-    EPS:float = 1e-12,
+    EPS: float = 1e-12,
 ) -> list[Vector]:
+    """
+    Calculate normalized vertex normals from triangle surface normals.
+    Surface normals are accumulated to every vertex and normalized.
+    IMPORTANT:
+        The returned normals are always unit vectors.
+    """
     normals = [
-        Vector((0.0,0.0,0.0))
+        Vector((0.0, 0.0, 0.0))
         for _ in range(vertex_count)
     ]
-    marker = [
-        False
-        for _ in range(vertex_count)
-    ]
-    ##################################################
-    # Build vertex -> triangles map
-    ##################################################
-    vertex_triangles = [
-        []
-        for _ in range(vertex_count)
-    ]
-    for tri_id in range(
-        len(surface_normals)
-    ):
+    for tri_id, n in enumerate(surface_normals):
         i = tri_id * 3
+        if i + 2 >= len(triangles):
+            break
         a = triangles[i]
-        b = triangles[i+1]
-        c = triangles[i+2]
-        vertex_triangles[a].append(tri_id)
-        vertex_triangles[b].append(tri_id)
-        vertex_triangles[c].append(tri_id)
-    ##################################################
-    # Accumulate normals
-    ##################################################
-    for tri_id,n in enumerate(surface_normals):
-        i = tri_id * 3
-        a = triangles[i]
-        b = triangles[i+1]
-        c = triangles[i+2]
-        if not marker[a]:
-            normals[a]=n.copy()
-            marker[a]=True
-        else:
-            normals[a]+=n
-        if not marker[b]:
-            normals[b]=n.copy()
-            marker[b]=True
-        else:
-            normals[b]+=n
-        if not marker[c]:
-            normals[c]=n.copy()
-            marker[c]=True
-        else:
-            normals[c]+=n
-    ##################################################
-    # fallback
-    ##################################################
+        b = triangles[i + 1]
+        c = triangles[i + 2]
+        if not (
+            0 <= a < vertex_count
+            and
+            0 <= b < vertex_count
+            and
+            0 <= c < vertex_count
+        ):
+            continue
+        # Ignore degenerate triangle
+        if n.length_squared <= EPS:
+            continue
+        normals[a] += n
+        normals[b] += n
+        normals[c] += n
+    #
+    # Normalize
+    #
+    fallback = Vector((0.0, 0.0, 1.0))
     for i in range(vertex_count):
-        if not marker[i]:
-            if i>0:
-                normals[i]=normals[i-1].copy()
-            else:
-                normals[i]=Vector((0,0,1))
-        elif normals[i].length_squared < EPS:
-            if i>0:
-                normals[i]=normals[i-1].copy()
-            else:
-                normals[i]=Vector((0,0,1))
+        length_squared = normals[i].length_squared
+        if length_squared > EPS:
+            normals[i].normalize()
+        else:
+            #
+            # IMPORTANT:
+            # Do NOT copy normals[i-1].
+            #
+            # That would introduce unrelated normal data
+            # from another vertex.
+            #
+            normals[i] = fallback.copy()
     return normals
 def recalculate_normals(
     triangles: list[int],
@@ -369,6 +350,13 @@ def recalculate_normals(
         triangles,
         surface_normals
     )
+    for i in range(min(10, len(vertex_normals))):
+        print(
+            i,
+            vertex_normals[i],
+            "length=",
+            vertex_normals[i].length
+        )
     return (
         vertex_normals,
         surface_normals,
@@ -379,22 +367,11 @@ def recalculate_tangents_fast(
     normals: list[Vector],
     uv: list[Vector],
 ):
-    """
-    Python port of:
-    RecalculateTangents.recalculateTangentsFast()
-    DAZ / VaM version
-    """
     num_vertices = len(vertices)
-    #
-    # Vector4 replacement
-    #
     tangents = [
-        Vector((0.0,0.0,0.0,0.0))
+        Vector((0.0, 0.0, 0.0, 0.0))
         for _ in range(num_vertices)
     ]
-    #
-    # markerArray
-    #
     marker = [
         False
         for _ in range(num_vertices)
@@ -402,70 +379,91 @@ def recalculate_tangents_fast(
     #
     # Triangle pass
     #
-    for i in range(0,len(triangles),3):
+    for i in range(
+        0,
+        len(triangles),
+        3
+    ):
         i1 = triangles[i]
-        i2 = triangles[i+1]
-        i3 = triangles[i+2]
-        #
-        # Position delta
-        #
-        x1 = vertices[i2].x - vertices[i1].x
-        y1 = vertices[i2].y - vertices[i1].y
-        z1 = vertices[i2].z - vertices[i1].z
-        x2 = vertices[i3].x - vertices[i1].x
-        y2 = vertices[i3].y - vertices[i1].y
-        z2 = vertices[i3].z - vertices[i1].z
-        #
-        # UV delta
-        #
-        s1 = uv[i2].x - uv[i1].x
-        s2 = uv[i3].x - uv[i1].x
-        t1 = uv[i2].y - uv[i1].y
-        t2 = uv[i3].y - uv[i1].y
-        #
-        # determinant
-        #
-        det = s1*t2 - s2*t1
-        #
-        # Official:
-        #
-        # if(num20==0)
-        #     num20=0.0001f
-        #
+        i2 = triangles[i + 1]
+        i3 = triangles[i + 2]
+        x1 = (
+            vertices[i2].x
+            -
+            vertices[i1].x
+        )
+        y1 = (
+            vertices[i2].y
+            -
+            vertices[i1].y
+        )
+        z1 = (
+            vertices[i2].z
+            -
+            vertices[i1].z
+        )
+        x2 = (
+            vertices[i3].x
+            -
+            vertices[i1].x
+        )
+        y2 = (
+            vertices[i3].y
+            -
+            vertices[i1].y
+        )
+        z2 = (
+            vertices[i3].z
+            -
+            vertices[i1].z
+        )
+        s1 = (
+            uv[i2].x
+            -
+            uv[i1].x
+        )
+        s2 = (
+            uv[i3].x
+            -
+            uv[i1].x
+        )
+        t1 = (
+            uv[i2].y
+            -
+            uv[i1].y
+        )
+        t2 = (
+            uv[i3].y
+            -
+            uv[i1].y
+        )
+        det = (
+            s1 * t2
+            -
+            s2 * t1
+        )
         if det == 0:
             det = 0.0001
         r = 1.0 / det
-        #
-        # tangent
-        #
         tx = (
-            (t2*x1 - t1*x2)
-            *
-            r
+            (t2 * x1 - t1 * x2)
+            * r
         )
         ty = (
-            (t2*y1 - t1*y2)
-            *
-            r
+            (t2 * y1 - t1 * y2)
+            * r
         )
         tz = (
-            (t2*z1 - t1*z2)
-            *
-            r
+            (t2 * z1 - t1 * z2)
+            * r
         )
-        tangent = Vector(
-            (
-                tx,
-                ty,
-                tz
-            )
-        )
-        #
-        # accumulate
-        #
-        for index in (i1,i2,i3):
+        for index in (
+            i1,
+            i2,
+            i3
+        ):
             if not marker[index]:
-                marker[index]=True
+                marker[index] = True
                 tangents[index].x = tx
                 tangents[index].y = ty
                 tangents[index].z = tz
@@ -474,7 +472,6 @@ def recalculate_tangents_fast(
                 tangents[index].y += ty
                 tangents[index].z += tz
     #
-    # Normalize
     # Gram-Schmidt
     #
     for k in range(num_vertices):
@@ -482,41 +479,23 @@ def recalculate_tangents_fast(
             continue
         n = normals[k]
         t = tangents[k]
-        #
-        # dot(normal,tangent)
-        #
         dot = (
-            n.x*t.x+
-            n.y*t.y+
-            n.z*t.z
+            n.x * t.x
+            +
+            n.y * t.y
+            +
+            n.z * t.z
         )
-        #
-        # tangent orthogonalize
-        #
         tangent = Vector(
             (
-                t.x - n.x*dot,
-                t.y - n.y*dot,
-                t.z - n.z*dot
+                t.x - n.x * dot,
+                t.y - n.y * dot,
+                t.z - n.z * dot
             )
         )
-        #
-        # normalize
-        #
-        # if tangent.length > 1e-12:
-        #     tangent.normalize()
-        # else:
-        #     tangent = Vector(
-        #         (1,0,0)
-        #     )
-        #
-        # official:
-        #
-        # tangents[k].w=-1
-        #
         tangents[k].x = tangent.x
         tangents[k].y = tangent.y
         tangents[k].z = tangent.z
         tangents[k].w = -1.0
-        marker[k]=False
+        marker[k] = False
     return tangents
